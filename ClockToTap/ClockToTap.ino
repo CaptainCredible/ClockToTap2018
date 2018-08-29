c82fe2a39baf5da99cdf300cd0af89b53efc435e
CaptainCredible <cpt.credible@gmail.com>
2018-08-29 08:26:44 +0200

    changes

--- ClockToTap/ClockToTap.ino
+++ ClockToTap/ClockToTap.ino
@@ -1,132 +1,154 @@
 /*
-CLOCK TO TAP
-Uses Arduino Leonardo bootloader and MIDIUSB library, oh and MIDI library (Francois Best)
+  CLOCK TO TAP
+  Uses Arduino Leonardo bootloader and MIDIUSB library, oh and MIDI library (Francois Best)
 */
 
+//#define proMicro
+
 //#include <ButtonDebounce.h>
-#include <MIDIUSB.h>
+//#include <MIDIUSB.h>
 #include <MIDI.h>
 
 bool intClock = true;
 bool notReceivedClockSinceBoot = true;
-bool littleButtStates[4] = { true,true,true,true };
-bool oldLittleButtStates[4] = { false,false,false,false };
-bool bigButtStates[4] = { true,true,true,true, };
-bool oldBigButtStates[4] = { false,false,false,false };
+bool littleButtStates[4] = { true, true, true, true };
+bool oldLittleButtStates[4] = { false, false, false, false };
+bool bigButtStates[4] = { true, true, true, true, };
+bool oldBigButtStates[4] = { false, false, false, false };
 unsigned long lastTimeOfTap = 0;
 unsigned long timeOfTap = 0;
 int clockStepTimer = 0;
-
-bool flippedTrips[4] = { false,false,false,false };
-unsigned long tripTimer[4] = { 0,0,0,0 };
-bool triplets[4] = { false,false,false,false };
+bool flippedTrips[4] = { false, false, false, false };
+unsigned long tripTimer[4] = { 0, 0, 0, 0 };
+bool triplets[4] = { false, false, false, false };
 //unsigned int bigButtTimer[4] = { 0,0,0,0 };
-const byte littleButtPins[4] = { 2,4,19,16 };
-const byte bigButtPins[4] = { 20,9,8,7 };
+const byte littleButtPins[4] = { PB0, PA6, PA2, PB9 };
+const byte bigButtPins[4] = { PA10, PB6, PB7, PB8 };
+byte waitLedSelect = 0;
+unsigned long waitTimer = 0;
+//int tapTimer = 0;
+unsigned long tapTimer = 0;
+unsigned long intClockTimer = 0;
+int tock = 0;
 
-MIDI_CREATE_DEFAULT_INSTANCE();
 
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial3, MIDI);
+
+
+#ifdef proMicro
 void noteOn(byte channel, byte pitch, byte velocity) {
-	midiEventPacket_t noteOn = { 0x09, 0x90 | channel, pitch, velocity };
-	MidiUSB.sendMIDI(noteOn);
+  midiEventPacket_t noteOn = { 0x09, 0x90 | channel, pitch, velocity };
+  MidiUSB.sendMIDI(noteOn);
 }
+
 
 void noteOff(byte channel, byte pitch, byte velocity) {
-	midiEventPacket_t noteOff = { 0x08, 0x80 | channel, pitch, velocity };
-	MidiUSB.sendMIDI(noteOff);
+  midiEventPacket_t noteOff = { 0x08, 0x80 | channel, pitch, velocity };
+  MidiUSB.sendMIDI(noteOff);
 }
 
-#define outA 18 //A0
-#define outB 15 //15
-#define outC 14 //14
-#define outD 21 //A3
+#endif
+
+
+//these need fixing
+#define outA PA1 //A0
+#define outB PA4 //15
+#define outC PA5 //14
+#define outD PB1 //A3
 byte outs[4] = { outA, outB, outC, outD };
 
-#define ledA 3 //D3
-#define ledB 5 //D3
-#define ledC 6 //D3
-#define ledD 10 //D3
+#define ledA PA1 //D3
+#define ledB PA4 //D3
+#define ledC PA5 //D3
+#define ledD PB1 //D3
 byte LEDs[4] = { ledA, ledB, ledC, ledD };
 
-unsigned long bigDebounceTimers[4] = { 0,0,0,0 };
-unsigned long smallDebounceTimers[4] = { 0,0,0,0 };
-bool bigDebounceReady[4] = { true,true,true,true };
-bool smallDebounceReady[4] = { true,true,true,true };
+
+unsigned long bigDebounceTimers[4] = { 0, 0, 0, 0 };
+unsigned long smallDebounceTimers[4] = { 0, 0, 0, 0 };
+bool bigDebounceReady[4] = { true, true, true, true };
+bool smallDebounceReady[4] = { true, true, true, true };
 
 
 const int debounceThresh = 10;
 
-bool inversion[4] = { false,false,false,false };
+
+bool inversion[4] = { false, false, false, false };
 
 void setup() {
-	//
-	MIDI.setHandleClock(MIDIClockTick);
-	MIDI.setHandleStart(MIDIStart);
-	MIDI.setHandleStop(handleStop);
-	MIDI.begin(MIDI_CHANNEL_OMNI); // Initiate MIDI communications, listen to all channels
-								   //Serial.begin(115200);
-	for (int i = 0; i < 4; i++) {
-		pinMode(outs[i], OUTPUT);
-		pinMode(LEDs[i], OUTPUT);
-		pinMode(littleButtPins[i], INPUT_PULLUP);
-		pinMode(bigButtPins[i], INPUT_PULLUP);
-		digitalWrite(LEDs[i], HIGH);
-	}
-	delay(500);
-	allLedsOff();
+  //
+  MIDI.setHandleClock(MIDIClockTick);
+  MIDI.setHandleStart(MIDIStart);
+  MIDI.setHandleStop(handleStop);
+  MIDI.begin(MIDI_CHANNEL_OMNI); // Initiate MIDI communications, listen to all channels
+  //Serial.begin(115200);
+  for (int i = 0; i < 4; i++) {
+    //pinMode(outs[i], OUTPUT);
+    //pinMode(LEDs[i], OUTPUT);
+    pinMode(littleButtPins[i], INPUT_PULLUP);
+    pinMode(bigButtPins[i], INPUT_PULLUP);
+    ////digitalWrite(LEDs[i], HIGH);
+    delay(200);
+  }
+  delay(500);
+  // ////digitalWrite(LEdD, HIGH);
+  // ////digitalWrite(LEdC, HIGH);
+  // ////digitalWrite(LEdB, HIGH);
+  // ////digitalWrite(LEdA, HIGH);
+  // pinMode(PC13, OUTPUT);
+  // digitalWrite(PC13, HIGH);
+  // delay(1000);
+  allLedsOff();
 }
 
 void allLedsOn() {
-	for (int i = 0; i < 4; i++) {
-		digitalWrite(LEDs[i], HIGH);
-	}
+  for (int i = 0; i < 4; i++) {
+    ////digitalWrite(LEDs[i], HIGH);
+  }
 }
 
 void allLedsOff() {
-	for (int i = 0; i < 4; i++) {
-		digitalWrite(LEDs[i], LOW);
-	}
-}
-byte waitLedSelect = 0;
-unsigned long waitTimer = 0;
-int tapTimer = 0;
-void waiting4clock() {
-	if (tapTimer > 0) {
-		//intClock = true;
-		handleIntClock();
-	}
+  for (int i = 0; i < 4; i++) {
+    ////digitalWrite(LEDs[i], LOW);
+  }
 }
 
-//unsigned long tapTimer = 0;
-unsigned long intClockTimer = 0;
-int tock = 0;
+
+void waiting4clock() {
+  if (tapTimer > 0) {
+    //intClock = true;
+    handleIntClock();
+  }
+}
+
+
 void handleIntClock() {
-	//Serial.println(tapTimer);
-	if (tapTimer > 0) {
-		//if (millis() - intClockTimer > tapTimer>>3) {
-		if (millis() - intClockTimer > clockStepTimer) {
-			//tock++;
-			//Serial.println(tock);
-			clockTick();
-			intClockTimer = millis();
-		}
-	}
+  //Serial.println("TapTimer = ");
+  //Serial.println(tapTimer);
+  //Serial.print("millis = ");
+  //Serial.println(millis());
+
+  if (tapTimer > 0) {
+    //if (millis() - intClockTimer > tapTimer>>3) {
+    if (millis() - intClockTimer > clockStepTimer) {
+      //tock++;
+      ////Serial.println(tock);
+      clockTick();
+      intClockTimer = millis();
+    }
+  }
 }
 
 void lightScroll() {
-	if (millis() - waitTimer > 60) {
-		waitTimer = millis();
-		digitalWrite(LEDs[waitLedSelect], LOW);
-		waitLedSelect++;
-		waitLedSelect = waitLedSelect % 4;
-		digitalWrite(LEDs[waitLedSelect], HIGH);
-	}
+  if (millis() - waitTimer > 60) {
+    waitTimer = millis();
+    ////digitalWrite(LEDs[waitLedSelect], LOW);
+    waitLedSelect++;
+    waitLedSelect = waitLedSelect % 4;
+    ////digitalWrite(LEDs[waitLedSelect], HIGH);
+  }
 }
 
-// First parameter is the event type (0x0B = control change).
-// Second parameter is the event type, combined with the channel.
-// Third parameter is the control number number (0-119).
-// Fourth parameter is the control value (0-127).
 
 
 
@@ -141,297 +163,194 @@ bool isRunning = false;
 bool clockUpdated = false;
 
 void MIDIClockTick() {
-	intClock = false;
-	notReceivedClockSinceBoot = false;
-	clockTick();
+  intClock = false;
+  notReceivedClockSinceBoot = false;
+  clockTick();
 }
 
 void clockTick() {
-	
-	clockIncrement++;
-	handleTaps();
+
+  clockIncrement++;
+  handleTaps();
 }
 
 void handleStop() {
-	notReceivedClockSinceBoot = false;
-	Serial.println("STOP RESET");
-	isRunning = false;
-	allLedsOff();
+  notReceivedClockSinceBoot = false;
+  //Serial.println("STOP RESET");
+  isRunning = false;
+  allLedsOff();
 }
 
 void MIDIStart() {
-	intClock = false;
-	notReceivedClockSinceBoot = false;
-	handleStart();
+  intClock = false;
+  notReceivedClockSinceBoot = false;
+  handleStart();
 }
 
 void handleStart() {
-	notReceivedClockSinceBoot = false;
-	Serial.println("START");
-	isRunning = true;
-	clockIncrement = 0;
-	handleTaps();
-	//clockTick();
+  notReceivedClockSinceBoot = false;
+  //Serial.println("START");
+  isRunning = true;
+  clockIncrement = 0;
+  handleTaps();
+  //clockTick();
 }
 byte tapBlinkLength = 3;
-int localTapTimer[4] = { 0,0,0,0 };
+int localTapTimer[4] = { 0, 0, 0, 0 };
 void handleTaps() {
-	//Serial.println("hello");
-	for (int i = 0; i < 4; i++) {
+  ////Serial.println("hello");
+  for (int i = 0; i < 4; i++) {
 
-		localTapTimer[i] = clockIncrement % clockLengths[i];
-		//	Serial.println(taps[i]);
+    localTapTimer[i] = clockIncrement % clockLengths[i];
+    //  //Serial.println(taps[i]);
 
 
-		if (localTapTimer[i] == 0) {
-			taps[i] = true;
-			digitalWrite(outs[i], !inversion[i]);
-			if (!bigButtStates[i]) {
-				digitalWrite(LEDs[i], !inversion[i]);
-			}
-		}
+    if (localTapTimer[i] == 0) {
+      taps[i] = true;
+      digitalWrite(outs[i], !inversion[i]);
+      if (!bigButtStates[i]) {
+        ////digitalWrite(LEDs[i], !inversion[i]);
+      }
+    }
 
-		else if (localTapTimer[i] == tapBlinkLength) {
-			taps[i] = false;
-			digitalWrite(outs[i], inversion[i]);
-			if (!bigButtStates[i]) {
-				digitalWrite(LEDs[i], inversion[i]);
-			}
-		}
-	}
+    else if (localTapTimer[i] == tapBlinkLength) {
+      taps[i] = false;
+      digitalWrite(outs[i], inversion[i]);
+      if (!bigButtStates[i]) {
+        ////digitalWrite(LEDs[i], inversion[i]);
+      }
+    }
+  }
 }
 
 
-
+int aliveCounter = 0;
 void loop() {
-	if (notReceivedClockSinceBoot) {
-		//waiting4clock();
-	}
+  aliveCounter++;
+  if (notReceivedClockSinceBoot) {
+    //waiting4clock();
+  }
 
-	if (intClock) {
-		handleIntClock();
-	}
+  if (intClock) {
+    handleIntClock();
+  }
 
-	MIDI.read();
-	midiEventPacket_t rx;
-	do {
-		rx = MidiUSB.read();
-		if (rx.header != 0) {
-			if (rx.header == 15) {
-				if (rx.byte1 == 248) {
-					
-					MIDIClockTick();
-				}
-				else if (rx.byte1 == 252) {
+  MIDI.read();
 
-					handleStop();
-				}
-			}
-			else if (rx.header == 3 && rx.byte1 == 242) { // a start 
-				intClock = false;
-				notReceivedClockSinceBoot = false;
-				handleStart();
 
-			}
-			/*
-			Serial.print("Received: ");
-			Serial.print(rx.header);
-			Serial.print("-");
-			Serial.print(rx.byte1);
-			Serial.print("-");
-			Serial.print(rx.byte2);
-			Serial.print("-");
-			Serial.println(rx.byte3);
-			*/
-		}
-	} while (rx.header != 0);
-	handleButts();
-	handleBlinks();
+  handleButts();
+  handleBlinks();
 }
 
 
 
 
 int temp = 0;
-void handleButts() {
-	for (int i = 0; i < 4; i++) {
-		oldLittleButtStates[i] = littleButtStates[i];
-
-		if (smallDebounceReady[i]) {
-			littleButtStates[i] = !digitalRead(littleButtPins[i]);
-		}
-		else {
-			debounce(0, i);
-		}
-
-		if (littleButtStates[i] && !oldLittleButtStates[i]) {
-			inversion[i] = !inversion[i];
-			smallDebounceTimers[i] = millis();
-			smallDebounceReady[i] = false;
-
-
-			if (localTapTimer[i] > tapBlinkLength) { //set LED imediately to show respect
-
-				digitalWrite(LEDs[i], inversion[i]);
-
-			}
-			else {
-				digitalWrite(LEDs[i], !inversion[i]);
-			}
-
-
-
-
-
-
-			Serial.print("inversion ");
-			Serial.print(i);
-			Serial.print(" = ");
-			Serial.println(inversion[i]);
-		}
-		else if (!littleButtStates[i] && oldLittleButtStates[i]) {
-			smallDebounceTimers[i] = millis();
-			smallDebounceReady[i] = false;
-		}
-	}
-
-	for (int i = 0; i < 4; i++) {
-		oldBigButtStates[i] = bigButtStates[i];
-		if (bigDebounceReady[i]) {
-			bigButtStates[i] = !digitalRead(bigButtPins[i]);
-		}
-		else {
-			debounce(1, i);
-		}
-
-		//Serial.println(flippedTrips[i]);
-
-
-
-		if (bigButtStates[i] && !oldBigButtStates[i]) {
-			digitalWrite(LEDs[i], HIGH);
-			tripTimer[i] = millis();
-			flippedTrips[i] = false;
-			bigDebounceTimers[i] = millis();
-			bigDebounceReady[i] = false;
-
-			Serial.print("bigButt - ");
-			Serial.println(i);
-
-			if ((i == 0) && intClock) {  //don't do this if we are handling last butt and we are in intClock
-				lastTimeOfTap = timeOfTap;
-				timeOfTap = millis();
-				if (timeOfTap - lastTimeOfTap < 3000) { //if less than 3 sec since last tap
-					tapTimer = timeOfTap - lastTimeOfTap;
-					clockStepTimer = tapTimer / 24;
-					handleStart();
-					Serial.print("CLOCKSTEPTIMER = ");
-					Serial.println(clockStepTimer);
-				}
-			}
-			else {
-				clockDivisors[i]++;
-				clockDivisors[i] = clockDivisors[i] % 4;
-				setClockLengths(i);
-			}
-
-		}
-		else if (!bigButtStates[i] && oldBigButtStates[i]) {
-			bigDebounceTimers[i] = millis();
-			bigDebounceReady[i] = false;
-			digitalWrite(LEDs[i], LOW);
-		}
-
-		if (bigButtStates[i]) {
-
-			if (millis() - tripTimer[i] > 300 && !flippedTrips[i]) {
-				Serial.println(millis() - tripTimer[i]);
-				triplets[i] = !triplets[i];
-				flippedTrips[i] = true;
-				clockDivisors[i]--;
-				if (clockDivisors[i] < 0) {
-					clockDivisors[i] = 3;
-				}
-				setClockLengths(i);
-				if (triplets[i]) {
-					blink(i, 3);
-				}
-				else {
-					blink(i, 1);
-				}
-
-
-				for (int pop = 0; pop < 4; pop++) {
-					Serial.print("triplets ");
-					Serial.print(pop);
-					Serial.print(" = ");
-					Serial.println(triplets[pop]);
-				}
-			}
-		}
-	}
-}
-
 void setClockLengths(byte tap) {
-	if (!triplets[tap]) {
-		clockLengths[tap] = 48 >> clockDivisors[tap];
-	}
-	else {
-		clockLengths[tap] = 32 >> clockDivisors[tap];
-	}
+  if (!triplets[tap]) {
+    clockLengths[tap] = 48 >> clockDivisors[tap];
+  }
+  else {
+    clockLengths[tap] = 32 >> clockDivisors[tap];
+  }
 }
 
 void debounce(bool bigSmall, byte number) {
-	/*Serial.print("deBouncing ");
-	Serial.print(bigSmall);
-	Serial.print(" - ");
-	Serial.print(number);
-	Serial.print(" - ");
-	Serial.print(smallDebounceTimers[number]);
-	Serial.print(" - ");
-	Serial.println(millis());
-	*/
+  /*//Serial.print("deBouncing ");
+    //Serial.print(bigSmall);
+    //Serial.print(" - ");
+    //Serial.print(number);
+    //Serial.print(" - ");
+    //Serial.print(smallDebounceTimers[number]);
+    //Serial.print(" - ");
+    //Serial.println(millis());
+  */
 
-	if (bigSmall) {  //if its a big button
+  if (bigSmall) {  //if its a big button
 
-		if (millis() - bigDebounceTimers[number] > debounceThresh) {
-			bigDebounceReady[number] = true;
-			//Serial.println("big debounce DONE");
-		}
-	}
-	else {
-		if (millis() - smallDebounceTimers[number] > debounceThresh) {
-			smallDebounceReady[number] = true;
-			//Serial.println("small debounce DONE");
-		}
-	}
+    if (millis() - bigDebounceTimers[number] > debounceThresh) {
+      bigDebounceReady[number] = true;
+      ////Serial.println("big debounce DONE");
+    }
+  }
+  else {
+    if (millis() - smallDebounceTimers[number] > debounceThresh) {
+      smallDebounceReady[number] = true;
+      ////Serial.println("small debounce DONE");
+    }
+  }
 }
 
 unsigned long blinkTimers[4];
 byte blinkCounter[4] = { 0, 0, 0, 0 };
 
 void blink(byte led, byte times) {
-	blinkCounter[led] = times + 1;
-	blinkTimers[led] = millis();
+  blinkCounter[led] = times + 1;
+  blinkTimers[led] = millis();
 }
 
 int desiredBlinkPeriod = 100;
 //bool onTimerSetButNotReached[4] = { false, false, false, false };
 void handleBlinks() {
-	for (int i = 0; i < 4; i++) {
-		if (blinkCounter[i] > 0) {
-			if (millis() - blinkTimers[i] < desiredBlinkPeriod >> 1) { //if we are b4 halfway point
-				digitalWrite(LEDs[i], HIGH);
-			}
-			else if (millis() - blinkTimers[i] < desiredBlinkPeriod) {
-				digitalWrite(LEDs[i], LOW);
-				Serial.println(blinkCounter[i]);
-			}
-			else {
-				blinkCounter[i]--;
-				blinkTimers[i] = millis();
-			}
-		}
-
-	}
+  for (int i = 0; i < 4; i++) {
+    if (blinkCounter[i] > 0) {
+      if (millis() - blinkTimers[i] < desiredBlinkPeriod >> 1) { //if we are b4 halfway point
+        ////digitalWrite(LEDs[i], HIGH);
+      }
+      else if (millis() - blinkTimers[i] < desiredBlinkPeriod) {
+        ////digitalWrite(LEDs[i], LOW);
+        //Serial.println(blinkCounter[i]);
+      }
+      else {
+        blinkCounter[i]--;
+        blinkTimers[i] = millis();
+      }
+    }
+  }
 }
+
+
+
+
+void proMicroUSB() {
+#ifdef proMicro
+
+  // First parameter is the event type (0x0B = control change).
+  // Second parameter is the event type, combined with the channel.
+  // Third parameter is the control number number (0-119).
+  // Fourth parameter is the control value (0-127).
+  midiEventPacket_t rx;
+  do {
+    rx = MidiUSB.read();
+    if (rx.header != 0) {
+      if (rx.header == 15) {
+        if (rx.byte1 == 248) {
+
+          MIDIClockTick();
+        }
+        else if (rx.byte1 == 252) {
+
+          handleStop();
+        }
+      }
+      else if (rx.header == 3 && rx.byte1 == 242) { // a start
+        intClock = false;
+        notReceivedClockSinceBoot = false;
+        handleStart();
+
+      }
+      /*
+        //Serial.print("Received: ");
+        //Serial.print(rx.header);
+        //Serial.print("-");
+        //Serial.print(rx.byte1);
+        //Serial.print("-");
+        //Serial.print(rx.byte2);
+        //Serial.print("-");
+        //Serial.println(rx.byte3);
+      */
+    }
+  } while (rx.header != 0);
+#endif
+}
+
